585c73b7b37cf8fa3ede85d19f7a9c5780b047e1
RohitMaurya139 <rohit139maurya@gmail.com>
2025-08-11 12:22:01 +0530

    update

---
+++ src/component/App.jsx
@@ -0,0 +1,13 @@
+
+
+function App() {
+  return (
+    <>
+      <h1 className="font-bold text-3xl text-red-600">hello world</h1>
+      <h1 className="font-bold text-3xl text-green-600">Rohit Maurya</h1>
+      <h1 className="font-bold text-3xl text-yellow-600">Namste Everyone Let's build Netflix GPT</h1>
+    </>
+  );
+}
+
+export default App;
